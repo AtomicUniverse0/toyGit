1302f55b2580f824a01c50c46402f976b5b281eb
lly_mac_ver <3190403700@qq.com>
2026-07-06 21:07:08 +0800

    feat: implemented GitRepository, which corresponding to a git object

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # toyGit
+
+一个简单的git实现，写着玩的，参考了https://riife.github.io/wyag-zh/docs/wyag-zh.html的教程
---
+++ libToyGit.py
@@ -0,0 +1,106 @@
+from configparser import ConfigParser
+import argparse
+import sys
+import os
+
+
+parser = argparse.ArgumentParser(description="Toy Git")
+subparser = parser.add_subparsers(title = "Commands", dest= "command")
+subparser.required = True
+
+sub_init = subparser.add_parser("init", help="初始化一个新的git仓库")
+sub_init.add_argument("path", nargs= "?", default= ".", help = "仓库所在的位置")
+
+
+class GitRepository:
+    worktree : str = None
+    gitdir : str= None
+    config : ConfigParser = None
+
+    """若git仓库不存在，则创建"""
+    def __init__(self, path: str):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+        
+        if not os.path.isdir(self.worktree):
+            raise RuntimeError("Invalid worktree: " + path)
+        
+        if not os.path.exists(self.gitdir):   # Git仓库不存在，需要新建
+            GitRepository._create_new_repo(path)
+        
+        assert os.path.isdir(self.gitdir), "Error: .git should be a directory"             
+
+        self.config = ConfigParser()
+        self.config.read( git_repo_file(self.gitdir, "config") )
+
+        version = int(self.config.get("core", "repositoryformatversion"))
+        assert version == 0, "Unsupported repositoryformatversion: " + str(version)
+    
+    """在path目录下创建一个新的git仓库，调用者需要保证path已经存在"""
+    @staticmethod
+    def _create_new_repo(path: str) -> None:
+        assert os.path.isdir(path)
+
+        git_repo_path = os.path.join(path, ".git")
+        assert git_repo_dir(git_repo_path, "branches", mkdir=True)
+        assert git_repo_dir(git_repo_path, "objects", mkdir=True)
+        assert git_repo_dir(git_repo_path, "refs", "tags", mkdir=True)
+        assert git_repo_dir(git_repo_path, "refs", "heads", mkdir=True)
+
+        with open( git_repo_file(git_repo_path, "description", mkdir=True), "w") as f:
+            f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+        
+        with open( git_repo_file(git_repo_path, "HEAD", mkdir=True), "w") as f:
+            f.write("ref: refs/heads/master\n")
+
+        with open( git_repo_file(git_repo_path, "config", mkdir=True), "w") as f:
+            config = ConfigParser()
+            config.add_section("core")
+            config.set("core", "repositoryformatversion", "0")
+            config.set("core", "filemode", "false")
+            config.set("core", "bare", "false")
+            config.write(f)
+
+
+""" 如果目录存在，则返回目录的地址，否则返回None """
+def git_repo_dir(git_repo_path: str, *path, mkdir : bool = False) -> str:
+
+    repo_dir_addr = os.path.join(git_repo_path, *path)
+    if os.path.exists(repo_dir_addr) :
+        assert os.path.isdir(repo_dir_addr), "Invalid argument: " + repo_dir_addr
+        return repo_dir_addr
+
+    if mkdir :
+        os.makedirs(repo_dir_addr, exist_ok=True)
+        return  repo_dir_addr
+    
+    return None
+
+""" 如果文件所属的目录存在，则返回文件的地址，否则返回None """
+def git_repo_file(git_repo_path: str, *path, mkdir : bool = False) -> str:
+    if git_repo_dir(git_repo_path, *path[:-1], mkdir=mkdir) is None :
+        return None
+    
+    # 运行到这里，说明目录一定存在，目录下的文件可能不存在
+    repo_file_addr = os.path.join(git_repo_path, *path)
+    if os.path.exists(repo_file_addr):
+        assert os.path.isfile(repo_file_addr), "Invalid argument: " + repo_file_addr
+
+    return repo_file_addr
+
+def cmd_init(args) -> None:
+    try:
+        print("初始化git仓库，路径为: %s" % args.path)
+        repo = GitRepository(args.path)
+        print(f"Initialized empty Git repository in {repo.gitdir}")
+    except RuntimeError as e:
+        print(e)
+
+def main(argv = sys.argv[1:]) -> None:
+    args = parser.parse_args(argv)
+    match args.command:
+        case "init" : 
+            cmd_init(args)
+        case _  : 
+            print("无效命令。")
+
